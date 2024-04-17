0f0c5b6702b1a2d60a7fffb5672cccab6bcec99d
Palak Rajput (DHL SG), external <palak.rajput@dhl.com>
2024-04-17 16:54:23 +0530

    StringQues

--- src/main/com/concept/stream/StringQues.java
+++ src/main/com/concept/stream/StringQues.java
@@ -1,8 +1,6 @@
 package src.main.com.concept.stream;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class StringQues {
@@ -29,18 +27,18 @@ public class StringQues {
      * <p>
      * All occurrences of a character must be replaced with another character while preserving the order of characters.
      * No two characters may map to the same character, but a character may map to itself.
-     *
-     *
+     * <p>
+     * <p>
      * Example 1:
-     *
+     * <p>
      * Input: s = "egg", t = "add"
      * Output: true
      * Example 2:
-     *
+     * <p>
      * Input: s = "foo", t = "bar"
      * Output: false
      * Example 3:
-     *
+     * <p>
      * Input: s = "paper", t = "title"
      * Output: true
      */
@@ -67,5 +65,31 @@ public class StringQues {
             }
             System.out.println("Are Strings isomorphic? " + result);
         }
+    }
+
+    /**
+     * Given a string s, find the first non-repeating character in it and return its index. If it does not exist, return -1.
+     *
+     * Input: s = "aabb"
+     * Output: -1
+     *
+     *
+     * @param s input
+     * @return index of first unique char
+     */
+    public int firstUniqChar(String s) {
+        char[] chars = s.toCharArray();
+        int[] count = new int[128];
+        for (char c : chars) {
+            count[c]++;
+        }
+        int index = 0;
+        for (char c : chars) {
+            if (count[c] == 1) {
+                return index;
+            }
+            index++;
+        }
+        return -1;
     }
 }
