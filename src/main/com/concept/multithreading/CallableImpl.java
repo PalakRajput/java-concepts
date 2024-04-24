80928e882d7fb4125ec4ab744ca88eb34cc63ae2
Palak Rajput (DHL SG), external <palak.rajput@dhl.com>
2024-04-24 10:52:55 +0530

    MultiThreading | Callable & Runnable

---
+++ src/main/com/concept/multithreading/CallableImpl.java
@@ -0,0 +1,33 @@
+package src.main.com.concept.multithreading;
+
+import java.util.concurrent.*;
+
+public class CallableImpl implements Callable<Integer> {
+    /**
+     * Computes a result, or throws an exception if unable to do so.
+     *
+     * @return computed result
+     * @throws Exception if unable to compute a result
+     */
+    @Override
+    public Integer call() throws Exception {
+        int sum = 0;
+        for (int i = 0; i < 10; i++) {
+            sum += i;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newFixedThreadPool(2);
+
+        Future<Integer> future = service.submit(new CallableImpl());
+
+        try {
+            int sum  = future.get();
+            System.out.println(sum);
+        } catch (InterruptedException | ExecutionException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ src/main/com/concept/multithreading/RunnableImpl.java
@@ -0,0 +1,52 @@
+package src.main.com.concept.multithreading;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class RunnableImpl implements Runnable {
+
+    /**
+     * When an object implementing interface {@code Runnable} is used
+     * to create a thread, starting the thread causes the object's
+     * {@code run} method to be called in that separately executing
+     * thread.
+     * <p>
+     * The general contract of the method {@code run} is that it may
+     * take any action whatsoever.
+     *
+     * @see Thread#run()
+     */
+    @Override
+    public void run() {
+        for(int i=0; i< 10; i++){
+            System.out.println(Thread.currentThread().getName() + " Run method called: " + i);
+//            System.out.println("Run method called: " + i);
+        }
+    }
+
+    public static void main(String[] args) {
+        Thread thread = new Thread(new RunnableImpl());
+        thread.start();
+
+        //The main method will keep on running unless the executor service is shut down.
+
+
+        //Create 5 threads
+        ExecutorService service = Executors.newFixedThreadPool(5);
+        Future<?> future = service.submit(new RunnableImpl());
+
+        //All queued/waiting tasks are completed but no new tasks are submitted.
+        service.shutdown();
+
+        //Only a single thread is created
+        ExecutorService service1 = Executors.newSingleThreadExecutor();
+        Runnable runnable = () -> System.out.println("Hey");
+        service1.execute(runnable);
+
+        //Returns list of waiting task and tries to shut down.
+        service1.shutdownNow();
+
+
+    }
+}
